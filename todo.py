2ce7952969048a7166f79c3d8253f531666e8673
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2016-05-06 22:02:45 -0300

    Remocao de linhas denecessárias

--- todo.py
+++ todo.py
@@ -21,15 +21,14 @@ class acesso_barramento():
             'Authorization': 'Token %s' % self.token
         }
 
-    @staticmethod
+    
     def get_municipios_por_uf(self, uf):
 
         response = requests.get(_url('/uf/%s/municipios' %uf), headers=self.headers)
         data = response.json()
         print data
         return data
-
-    @staticmethod
+    
     def get_uf(self):
 
         response = requests.get(_url('/uf/'), headers=self.headers)
@@ -37,169 +36,135 @@ class acesso_barramento():
         print data
         return data
 
-    ####################################################################################################################
-    ############################################METODOS DOS TIPOS#######################################################
-    ####################################################################################################################
-    @staticmethod
+####################################################################################################################
+###########################################METODOS DOS TIPOS#######################################################
+#################################################################################################################### 
     def get_tipoestabelecimento(self):
         response = requests.get(_url('tipos/tipoestabelecimento/'), headers=self.headers)
         data = response.json()
         print data
         return data
-
-    @staticmethod
+    
     def get_subtipoestabelecimento(self):
         response = requests.get(_url('tipos/subtipoestabelecimento/'), headers=self.headers)
         data = response.json()
         print data
         return data
-
-    @staticmethod
+    
     def get_tipogestao(self):
         response = requests.get(_url('tipos/tipogestao/'), headers=self.headers)
         data = response.json()
         print data
         return data
-
-    @staticmethod
+    
     def get_cbo(self):
         response = requests.get(_url('tipos/cbo/'), headers=self.headers)
         data = response.json()
         print data
         return data
-        pass
-
-    @staticmethod
+    
     def get_tipotelefone(self):
         response = requests.get(_url('tipos/tipotelefone/'), headers=self.headers)
         data = response.json()
         print data
         return data
-        pass
-
-    @staticmethod
+    
     def get_tipoequipe(self):
         response = requests.get(_url('tipos/tipoequipe/'), headers=self.headers)
         data = response.json()
         print data
         return data
-        pass
-
-    @staticmethod
+    
     def get_esferaadministrativa(self):
         response = requests.get(_url('tipos/esferaadministrativa/'), headers=self.headers)
         data = response.json()
         print data
         return data
-        pass
-
-    @staticmethod
+    
     def get_naturezaorganizacao(self):
         response = requests.get(_url('tipos/naturezaorganizacao/'), headers=self.headers)
         data = response.json()
         print data
         return data
-        pass
-
-    @staticmethod
+    
     def get_vinculoempregaticio(self):
         response = requests.get(_url('tipos/vinculoempregaticio/'), headers=self.headers)
         data = response.json()
         print data
         return data
-        pass
-
-    @staticmethod
+    
     def get_tipovinculoempregaticio(self):
         response = requests.get(_url('tipos/tipovinculoempregaticio/'), headers=self.headers)
         data = response.json()
         print data
         return data
-        pass
-
-    @staticmethod
+    
     def get_subtipovinculoempregaticio(self):
         response = requests.get(_url('tipos/subtipovinculoempregaticio/'), headers=self.headers)
         data = response.json()
         print data
         return data
-        pass
-
-    @staticmethod
+    
     def get_tipoinscricaoprofissional(self):
         response = requests.get(_url('tipos/tipoinscricaoprofissional/'), headers=self.headers)
         data = response.json()
         print data
         return data
-        pass
-
-    @staticmethod
+    
     def get_tipoespecialidade(self):
         response = requests.get(_url('tipos/tipoespecialidade/'), headers=self.headers)
         data = response.json()
         print data
         return data
-        pass
-
-    @staticmethod
+    
     def get_tipoinscricao(self):
         response = requests.get(_url('tipos/tipoinscricao/'), headers=self.headers)
         data = response.json()
         print data
         return data
-        pass
-
-    @staticmethod
+    
     def get_especialidadeambulatorial(self):
         response = requests.get(_url('tipos/especialidadeambulatorial/'), headers=self.headers)
         data = response.json()
         print data
         return data
-        pass
-
-    @staticmethod
+    
     def get_fonteinformacao(self):
         response = requests.get(_url('tipos/fonteinformacao/'), headers=self.headers)
         data = response.json()
         print data
         return data
-        pass
-
-    @staticmethod
+    
     def get_tipoequipamento(self):
         response = requests.get(_url('tipos/tipoequipamento/'), headers=self.headers)
         data = response.json()
         print data
         return data
-        pass
-
-    @staticmethod
+    
     def get_especialidade(self):
         response = requests.get(_url('tipos/especialidade/'), headers=self.headers)
         data = response.json()
         print data
         return data
 
-    ####################################################################################################################
-    ############################################METODOS DA REGIAO DE SAUDE##############################################
-    ####################################################################################################################
-    @staticmethod
+####################################################################################################################
+############################################METODOS DA REGIAO DE SAUDE##############################################
+####################################################################################################################
     def get_regiaosaude(self):
         response = requests.get(_url('/regiaosaude/'), headers=self.headers)
         data = response.json()
         print data
         return data
 
-    ####################################################################################################################
-    ############################################METODOS DA REGIAO GEOGRAFICA############################################
-    ####################################################################################################################
-    @staticmethod
+########################################################################################################################
+################################################METODOS DA REGIAO GEOGRAFICA############################################
+########################################################################################################################  
     def get_regiaogeografica(self):
         response = requests.get(_url('/regiaogeografica/'), headers=self.headers)
         data = response.json()
         print data
         return data
 
-    ####################################################################################################################
-    ############################################METODOS DO PROFISSIONAL#################################################
-    ####################################################################################################################
+####################################################################################################################
+############################################METODOS DO PROFISSIONAL#################################################
+####################################################################################################################
