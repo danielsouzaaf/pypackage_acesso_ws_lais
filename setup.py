f04a535ce7d1bd7120f83f87162f7dae473cda69
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2016-05-11 10:52:20 -0300

    Correcao do setu.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
+install_requires = [
+    'requests>=2.2.1',
+    ]
 setup(
     name = 'acesso_ws_lais',
     packages = ['acesso_ws_lais'], # this must be the same as the name above
     version = '0.1',
-    install_requires = [
-    'requests>=2.2.1',
-    ],
+
     description = 'Biblioteca para facilitar o acesso ao ws do LAIS.',
     author = 'Daniel Souza Affonso Ferreira',
     author_email = 'daniel.souza.af@gmail.com',
